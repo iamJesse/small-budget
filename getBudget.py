537002364b1e5dfe15ff8c955ada7703dc1a9d56
ivaylo Spasov <ivaylo.pif@gmail.com>
2014-07-07 15:20:02 +0300

    test csv support

--- getBudget.py
+++ getBudget.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python3
 
-__author__ = 'ivaylo'
+__author__ = 'ivaylo spasov'
+
+import os
+import csv
+import random
+import string
+
+filename = 'budget.csv'
+dataDir = 'data'
 
 
-def getBudget():
-    openBudget = open('budget.txt', 'w')
-    addToBudget = float(input('enter float: '))
-    currentBudget = openBudget.write(str(addToBudget))
-    print('Current budget is {0}'.format(addToBudget))
-    openBudget.close()
+def get_file_path(filename):
+    currenDirPath = os.getcwd()
+#   print(currenDirPath)
+    file_path = os.path.join(currenDirPath, dataDir, filename)
+#   print(file_path)
+    return file_path
 
 
-getBudget()
+path = get_file_path(filename)
+
+
+def read_csv(filepath):
+    with open(filepath, 'rU') as csvfile:
+        reader = csv.reader(csvfile)
+#       print(reader)
+        for row in reader:
+            print(row[0])
+#           print(float(row[0]))
+#           print(type(float(row[0])))
+
+
+read_csv(path)
